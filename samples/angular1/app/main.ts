14a16df030bf446f0b4fe51bfe6b7de0d6949711
ocalvo <ori.calvo@gmail.com>
2017-03-23 01:33:35 +0200

    Fixing angular1 sample

--- samples/angular1/app/main.ts
+++ samples/angular1/app/main.ts
@@ -1,41 +1,39 @@
-require("angular");
+import * as angular from "angular";
+import {AppComponent} from "./components/app.component";
+import {appModule} from "./app.module";
+import {AppStore} from "t-rex/AppStore";
+import {ContactListComponent} from "./components/contactList.component";
+import {AddContactComponent} from "./components/addContact.component";
+import {createLogger} from "t-rex/logger";
+import {AppState, RootService} from "./services/root.service";
+import {ContactService} from "./services/contacts.service";
 
-// import * as angular from "angular";
-// import {AppComponent} from "./components/app.component";
-// import {appModule} from "./app.module";
-// import {AppStore} from "t-rex/AppStore";
-// import {ContactListComponent} from "./components/contactList.component";
-// import {AddContactComponent} from "./components/addContact.component";
-// import {createLogger} from "t-rex/logger";
-// import {AppState, RootService} from "./services/root.service";
-// import {ContactService} from "./services/contacts.service";
-//
-// const logger = createLogger("main");
-//
-// const components = [
-//     AppComponent,
-//     ContactListComponent,
-//     AddContactComponent,
-// ];
-//
-// const services = [
-//     RootService,
-//     ContactService
-// ];
-//
-// appModule.run(($rootScope, rootService: RootService, contactService: ContactService) => {
-//     const appStore = new AppStore<AppState>();
-//
-//     appStore.subscribe((newState,oldState)=> {
-//         logger.log("$applyAsync after appState changed");
-//
-//         $rootScope.$applyAsync();
-//     });
-//
-//     appStore.init([
-//         rootService,
-//         contactService,
-//     ]);
-// });
-//
-// angular.bootstrap(document.getElementById("html"), [appModule.name]);
+const logger = createLogger("main");
+
+const components = [
+    AppComponent,
+    ContactListComponent,
+    AddContactComponent,
+];
+
+const services = [
+    RootService,
+    ContactService
+];
+
+appModule.run(($rootScope, rootService: RootService, contactService: ContactService) => {
+    const appStore = new AppStore<AppState>();
+
+    appStore.subscribe((newState,oldState)=> {
+        logger.log("$applyAsync after appState changed");
+
+        $rootScope.$applyAsync();
+    });
+
+    appStore.init([
+        rootService,
+        contactService,
+    ]);
+});
+
+angular.bootstrap(document.getElementById("html"), [appModule.name]);
