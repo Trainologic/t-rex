04e7d3c92b3d8b21d5f74f6cc33e1ff66f0573d8
ocalvo <ori.calvo@gmail.com>
2017-06-14 23:29:10 +0300

    Adding ActivityScope

---
+++ src/fx/ActivityScope.spec.ts
@@ -0,0 +1,391 @@
+import {configure} from "./config";
+
+configure({
+    enableLogging: false,
+    //activityAutoBeginTransaction: false,
+});
+
+import {AppStore} from "./AppStore";
+import {ServiceStore} from "./ServiceStore";
+import {Activity, transaction} from "./decorators";
+import {collectValues} from "../spec/collectValues";
+import {toBeEqualArray} from "../spec/toBeEqualArray";
+import {toDeeplyEqual} from "../spec/toDeeplyEqual";
+import {TransactionScope} from "./TransactionScope";
+
+describe("ServiceStore", function() {
+    interface AppState {
+        counters: CounterState,
+    }
+
+    class RootService {
+        store: ServiceStore<AppState> = ServiceStore.create<AppState>("/", {
+            counters: null,
+        });
+
+        constructor(private counterStore: CounterService, private authStore: AuthService) {
+        }
+
+        get state() {
+            return this.store.getState();
+        }
+
+        @Activity()
+        async incAndFail() {
+            this.counterStore.inc();
+            throw new Error("Ooops");
+        }
+
+        @Activity()
+        async incAndLogin(userName: string) {
+            this.counterStore.inc();
+            await this.authStore.login(userName);
+        }
+    }
+
+    interface CounterState {
+        value: number;
+    }
+
+    class CounterService {
+        store: ServiceStore<CounterState> = ServiceStore.create<CounterState>("counters", {
+            value: 0,
+        });
+
+        get state() {
+            return this.store.getState();
+        }
+
+        @Activity()
+        inc() {
+            this.store.update({
+                value: this.state.value + 1,
+            });
+        }
+    }
+
+    interface AuthState {
+        userName: string;
+        roles: string[];
+    }
+
+    class AuthService {
+        store: ServiceStore<AuthState> = new ServiceStore<AuthState>({
+            path: "auth",
+            initialState: {
+                userName: null,
+                roles: [],
+            }
+        });
+
+        get state() {
+            return this.store.getState();
+        }
+
+        @Activity()
+        login(userName: string) {
+            this.store.update({
+                userName: userName,
+            });
+        }
+
+        @Activity()
+        loginAndRunCallback(userName: string, callback) {
+            this.store.update({
+                userName: userName,
+            });
+
+            callback();
+            //setTimeout(callback, 100);
+        }
+
+        @Activity()
+        logout() {
+            this.store.update({
+                userName: null,
+                roles: [],
+            });
+        }
+
+        @Activity()
+        loadRoles() {
+            this.store.update({
+                roles: ["admin"],
+            });
+        }
+    }
+
+    let appStore: AppStore<AppState>;
+    let counterService: CounterService;
+    let authService: AuthService;
+    let rootService: RootService;
+
+    beforeEach(function() {
+        jasmine.addMatchers({
+            toBeEqualArray: toBeEqualArray,
+            toDeeplyEqual: toDeeplyEqual
+        });
+
+        appStore = new AppStore<AppState>();
+        counterService = new CounterService();
+        authService = new AuthService();
+        rootService = new RootService(counterService, authService);
+
+        appStore.init([
+            rootService,
+            authService,
+            counterService
+        ]);
+    });
+
+    it("with @Activity automatically commits changes to appStore", async function(done) {
+        await counterService.inc();
+        expect(rootService.state.counters.value).toBe(1);
+
+        done();
+    });
+
+    it("Supports nested trasactions", async function(done) {
+        await rootService.incAndLogin("Ori");
+
+        expect(rootService.state).toDeeplyEqual({
+            counters: {
+                value: 1,
+            },
+            auth: {
+                userName: "Ori",
+                roles: [],
+            }
+        });
+
+        done();
+    });
+
+    it("No commit in case of exception", async function(done) {
+        const beforeState = collectValues(rootService.state);
+        try {
+            await rootService.incAndFail();
+        }
+        catch(err) {
+        }
+        const afterState = collectValues(rootService.state);
+
+        expect(afterState).toBeEqualArray(beforeState);
+
+        done();
+    });
+
+    it("Does not allow second commit", async function(done) {
+        let tranScope;
+        await authService.loginAndRunCallback("userName", function() {
+            tranScope = TransactionScope.current();
+        });
+
+        expect(() => {
+            tranScope.commit();
+        }).toThrow(new Error("Activity was already committed"));
+
+        done();
+    });
+
+    it("subscribeTo fires when specific property has changed", async function (done) {
+        let fired = false;
+        authService.store.subscribeTo("userName", (newState, oldState)=> {
+            fired = true;
+        });
+
+        await authService.login("Ori");
+
+        //await authStore.logout();
+
+        expect(fired).toBe(true);
+        //expect(authStore.state).toEqual({userName: null, roles: []});
+
+        done();
+    });
+
+    it("subscribeTo does not fire on specific property that was not changed", async function (done) {
+        let fired;
+        authService.store.subscribeTo("userName", (newState, oldState)=> {
+            fired = true;
+        });
+        fired = false;
+
+
+        expect(fired).toBe(false);
+
+        done();
+    });
+
+    it("raises concurrency error when an array is modified by two parallel activities", async function (done) {
+        interface AppState {
+            nums: number[];
+        }
+
+        function delay(ms) {
+            return new Promise((resolve, reject)=> {
+                setTimeout(function() {
+                    resolve();
+                }, ms);
+            });
+        }
+
+        class Service1 {
+            store = ServiceStore.create<AppState>("/", {
+                nums: [],
+            });
+
+            get state() {
+                return this.store.getState();
+            }
+
+            @Activity()
+            async run() {
+                await delay(0);
+
+                await this.store.update({
+                    nums: this.state.nums.concat([1])
+                })
+            }
+        }
+
+        const appStore = new AppStore<AppState>();
+        const service1 = new Service1();
+        appStore.init([
+            service1
+        ]);
+
+        let thrown = false;
+        try {
+            await Promise.all([
+                service1.run(),
+                service1.run()
+            ]);
+        }
+        catch(err) {
+            thrown = true;
+        }
+
+        expect(thrown).toEqual(true);
+
+        done();
+    });
+
+    it("does not raise error when parallel transactions update different branches", async function (done) {
+        interface Branch1State {
+            counter: number;
+        }
+
+        interface Branch2State {
+            counter: number;
+        }
+
+        interface AppState {
+            branch1: Branch1State;
+            branch2: Branch2State;
+        }
+
+        function delay(ms) {
+            return new Promise((resolve, reject)=> {
+                setTimeout(function() {
+                    resolve();
+                }, ms);
+            });
+        }
+
+        class Service1 {
+            store = ServiceStore.create<Branch1State>("branch1", {
+                counter: 0,
+            });
+
+            get state() {
+                return this.store.getState();
+            }
+
+            @Activity()
+            async inc() {
+                await delay(0);
+
+                await this.store.update({
+                    counter: this.state.counter + 1
+                })
+            }
+        }
+
+        class Service2 {
+            store = ServiceStore.create<Branch2State>("branch2", {
+                counter: 0,
+            });
+
+            get state() {
+                return this.store.getState();
+            }
+
+            @Activity()
+            async inc() {
+                await delay(0);
+
+                await this.store.update({
+                    counter: this.state.counter + 1
+                })
+            }
+        }
+
+        const appStore = new AppStore<AppState>();
+        const service1 = new Service1();
+        const service2 = new Service2();
+        appStore.init([
+            service1,
+            service2,
+        ]);
+
+        let thrown = false;
+        try {
+            await Promise.all([
+                service1.inc(),
+                service2.inc()
+            ]);
+        }
+        catch(err) {
+            thrown = true;
+        }
+
+        expect(thrown).toEqual(false);
+
+        done();
+    });
+
+    it("notifies listeners when change occurs", function() {
+        let notified: boolean = false;
+
+        appStore.subscribe(()=> {
+            notified = true;
+        });
+
+        transaction(appStore, ()=> {
+            counterService.store.update({
+                value: counterService.state.value + 1,
+            });
+        });
+
+        expect(notified).toBe(true);
+    });
+
+    it("does not notify listener which unsubscribe", function() {
+        let notified: boolean = false;
+
+        const off = appStore.subscribe(()=> {
+            notified = true;
+        });
+
+        off();
+
+        transaction(appStore, ()=> {
+            counterService.store.update({
+                value: counterService.state.value + 1,
+            });
+        });
+
+        expect(notified).toBe(false);
+    });
+});
---
+++ src/fx/ActivityScope.ts
@@ -0,0 +1,155 @@
+import {AppStore} from "./AppStore";
+import {TransactionalObject} from "./TransactionalObject";
+import {createLogger, Logger} from "./logger";
+
+if(typeof Zone === "undefined") {
+    throw new Error("t-rex cannot execute without zone.js. Please ensure zone.js is loaded before t-rex");
+}
+
+export class TransactionScope {
+    private appStore: AppStore<any>;
+    private tranState: TransactionalObject<any>;
+    private committed: boolean;
+    private outerZone: Zone;
+    private id: number;
+    private logger: Logger;
+    private updateCount: number;
+
+    private static nextTranId = 0;
+
+    constructor(appStore: AppStore<any>) {
+        this.id = ++TransactionScope.nextTranId;
+        this.logger = createLogger("TransactionScope(" + this.id + ")");
+        this.updateCount = 0;
+        this.appStore = appStore;
+        this.tranState = new TransactionalObject(appStore.getState());
+        this.committed = false;
+        this.outerZone = Zone.current;
+
+        this.logger.log("created");
+    }
+
+    public update(path: string, changes: any) {
+        ++this.updateCount;
+        this.logger.log("update", path, changes);
+
+        this.ensureNotCommitted();
+
+        this.tranState.setProperty(path, changes);
+    }
+
+    public getNewState() {
+        return this.tranState.getCurrent();
+    }
+
+    public getOldState() {
+        return this.tranState.getBase();
+    }
+
+    public commit() {
+        this.ensureNotCommitted();
+
+        let oldState = this.tranState.getBase();
+        let newState = this.tranState.getCurrent();
+        const currentState = this.appStore.getState();
+
+        if(newState == currentState) {
+            this.logger.log("Nothing new to commit. updateCount is", this.updateCount);
+            return;
+        }
+
+        if(oldState != currentState) {
+            //
+            //  A parallel transaction has already modified the main store before us
+            //  We only allow parallel changes at different branches. Else, "Concurrency error is raised"
+            //
+            this.tranState.rebase(currentState);
+
+            oldState = this.tranState.getBase();
+            newState = this.tranState.getCurrent();
+        }
+
+        this.committed = true;
+
+        this.outerZone.run(()=> {
+            //
+            //  Committing to appStore causes emitting of change event
+            //  Subscribers must be notified outside of the transaction zone, else, any
+            //  additional update will be considered as part of the already committed transaction
+            //  and therefore will throw error
+            //
+            this.appStore.commit(oldState, newState);
+        });
+
+        //
+        //  Cleans management flags + switch between old and new
+        //
+        this.tranState.commit();
+
+        this.logger.log("committed");
+    }
+
+    static current(): TransactionScope {
+        let tran: TransactionScope = Zone.current.get("tran");
+        return tran;
+    }
+
+    static runInsideTransaction<StateT>(appStore: AppStore<any>, action) {
+        function runAction(func, commit) {
+            var retVal = func();
+            if(retVal && retVal.then) {
+                const promise = retVal;
+                return promise.then(res => {
+                    if(commit) {
+                        tran.commit();
+                    }
+
+                    return res;
+                });
+            }
+            else {
+                if(commit) {
+                    tran.commit();
+                }
+
+                return retVal;
+            }
+        }
+
+        let tran: TransactionScope = TransactionScope.current();
+        if(tran) {
+            tran.ensureNotCommitted();
+
+            //
+            //  This is a nested transaction
+            //  No need to commit changes to app base
+            //
+            return runAction(action, false);
+        }
+
+        //
+        //  This is a root transaction
+        //  When completed need to commit to the appStore
+        //
+        tran = new TransactionScope(appStore);
+
+        const spec: ZoneSpec = {
+            name: "tran",
+            properties: {
+                "tran": tran,
+            },
+        };
+
+        const zone = Zone.current.fork(spec);
+        return zone.run(function () {
+            var tran1 = TransactionScope.current();
+            return runAction(action, true);
+        });
+    }
+
+    private ensureNotCommitted() {
+        if(this.committed) {
+            throw new Error("Activity was already committed");
+        }
+    }
+}
